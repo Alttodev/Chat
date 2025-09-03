3a2169c1f96227e88521382e3fe2b94c7dc1233e
Alttodev <alttodev@gmail.com>
2025-09-03 07:55:16 +0530

    base setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-
   return (
     <>
-      chat
-      
+      <div className="text-green-400 flex justify-center items-center">
+        chat app
+      </div>
+      <div className="flex justify-center items-center">
+        <Button className="bg-emerald-600 hover:bg-emerald-600 text-white cursor-pointer">
+          Click me
+        </Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#1E40AF",
+        danger: "#DC2626",
+      },
+    },
+  },
+  plugins: [],
+};
