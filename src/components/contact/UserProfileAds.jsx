4346b0d96d88fbc51b727a68e31f1647bb2fc75f
Alttodev <alttodev@gmail.com>
2026-04-27 23:40:21 +0530

    updates

--- src/components/contact/UserProfileAds.jsx
+++ src/components/contact/UserProfileAds.jsx
@@ -1,7 +1,8 @@
+import image from "@/assets/message.png";
 export function UserProfileAds() {
   return (
     <div className="mt-20 ">
-      <img src="/src/assets/message.png" />
+      <img src={image} />
     </div>
   );
 }
