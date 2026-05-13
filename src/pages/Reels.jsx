3087fd34761f3d2c874bc22d18c94c581d83200b
Alttodev <alttodev@gmail.com>
2026-05-13 22:45:28 +0530

    reels page

--- src/pages/Reels.jsx
+++ src/pages/Reels.jsx
@@ -4,7 +4,7 @@ import { ShareDialog } from "@/components/modals/shareModal";
 
 export default function Reels() {
   return (
-    <div className="min-h-[calc(100vh-8rem)] pb-20 sm:pb-6">
+    <div className="min-h-[calc(100vh-8rem)] pb-2 sm:pb-6">
       <ReelsFeed />
       <ShareDialog />
     </div>
