40664c69840329d6d414771be8f5b31cfa252500
Alttodev <alttodev@gmail.com>
2026-05-25 11:56:03 +0530

    comment section update

--- src/components/skeleton/commentSkeleton.jsx
+++ src/components/skeleton/commentSkeleton.jsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 export function SkeletonComment() {
   return (
     <div className="flex items-center space-x-4">
-      <Skeleton className="h-12 w-12 rounded-full" />
+      <Skeleton className="h-8 w-8 rounded-full" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-[250px]" />
         <Skeleton className="h-4 w-[200px]" />
